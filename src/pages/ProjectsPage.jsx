1742fe4004ee06fbc0be201848c4776319e75675
WallochMatt <matthewrwalloch@gmail.com>
2023-07-07 16:23:26 -0500

    feat: Added HTML to ProjectsPage + Fixed the images to load properly TODO Create proper descriptions

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,9 +1,79 @@
 
 const ProjectsPage = () => {
     return ( 
-        <div>
-            <p>Hello from projects page!</p>
-        </div>
+        <main className="master-align projPage">
+            <div>
+                <div className="project">
+                    <h3>Combat Consensus - A scoring consensus application for combat sports</h3>
+                    
+                    <div className="prj-info">
+                        <div>
+                            <a href="http://3.143.230.10/" target="_blank">
+                                <img src={require("../assets/CC-example-1.png")}></img>
+                                
+                            </a>
+            
+                            <a href="https://www.youtube.com/watch?v=fEtIASFDeNY" target="_blank">
+                                <div className="youtube-background">
+                                    <img src={require("../assets/CC-example-3.png")}></img>
+                                    <div className="youtube-icon">
+                                        <i className="fa-brands fa-youtube fa-4x" style={{color: "#ff0000"}}></i>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+            
+                        <div>
+                            <p>Example text
+                                This example would have psuedo technical documentaion. A more in depth article about the project
+                            </p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consectetur libero id faucibus nisl tincidunt eget nullam non. Arcu bibendum at varius vel pharetra. Tellus cras adipiscing enim eu turpis egestas pretium aenean. Pulvinar sapien et ligula ullamcorper malesuada. Faucibus turpis in eu mi bibendum. Sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus. Fames ac turpis egestas integer eget aliquet nibh praesent tristique. Commodo quis imperdiet massa tincidunt nunc pulvinar. Nisi vitae suscipit tellus mauris a. Ultrices tincidunt arcu non sodales neque sodales ut. Rutrum tellus pellentesque eu tincidunt. Purus semper eget duis at tellus. Malesuada fames ac turpis egestas maecenas. Sit amet est placerat in egestas erat.
+            
+                                Nunc congue nisi vitae suscipit tellus mauris a diam maecenas. Augue neque gravida in fermentum et sollicitudin. Cras ornare arcu dui vivamus arcu felis. Venenatis lectus magna fringilla urna porttitor rhoncus dolor purus. Accumsan in nisl nisi scelerisque eu ultrices vitae auctor. Orci ac auctor augue mauris augue. Rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Vitae aliquet nec ullamcorper sit amet risus nullam eget. Ipsum consequat nisl vel pretium. Eros donec ac odio tempor orci dapibus ultrices. Tempor orci eu lobortis elementum nibh tellus molestie. Dolor magna eget est lorem ipsum dolor sit amet consectetur. Pellentesque elit eget gravida cum sociis natoque penatibus et. Ornare lectus sit amet est placerat in egestas erat imperdiet. Sit amet purus gravida quis blandit turpis cursus. Tristique senectus et netus et. Fames ac turpis egestas maecenas pharetra convallis posuere morbi leo.
+                                
+                                Massa eget egestas purus viverra accumsan in.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            
+                <div className="spacer"></div>
+            
+                <div className="project">
+                    <h3>CaraCara - An eCommerce Site</h3>
+            
+                    <div className="prj-info">
+                        <div>
+                            <a href="http://18.119.156.5/index.html#home_jump" target="_blank">
+                                <img src={require("../assets/CaraCara-example-2.png")}></img>
+                                <img src={require("../assets/CaraCara-example-3.png")}></img>
+                            </a>
+                        </div>
+            
+                        <div>
+                            <p>Example text of an explaination for the project, same infor as the lorem ispum above simulates</p>
+                        </div>    
+                    </div>
+                </div>
+            
+                <div className="spacer"></div>
+            
+                <div className="project">
+                <h3>uToob - A YouTube Parallel</h3>
+                    <div className="prj-info">
+                        <div>
+                            <a href="http://18.222.171.6/" target="_blank">
+                                <img src={require("../assets/uToob-example-1.png")}></img>
+                                <img src={require("../assets/uToob-example-2.png")}></img>
+                            </a>
+                        </div>
+                        <div>
+                            <p>Example text of an explaination for the project, same infor as the lorem ispum above simulates</p>
+                        </div>    
+                    </div>      
+                </div>
+            </div>
+        </main>
     );
 }
 
