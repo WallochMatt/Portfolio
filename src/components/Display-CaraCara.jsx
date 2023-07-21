52bf0a1837110b525479ee8d05f98b97a270e9ca
WallochMatt <matthewrwalloch@gmail.com>
2023-07-21 17:14:29 -0500

    refactor: Altered the other two displays to reflect the changes made in the Display-ComCon

--- src/components/Display-CaraCara.jsx
+++ src/components/Display-CaraCara.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const DisplayCaraCara = () => {
     return ( 
         <div className="project">
@@ -5,10 +7,19 @@ const DisplayCaraCara = () => {
     
             <div className="prj-info">
                 <div>
-                    <a href="http://18.119.156.5/index.html#home_jump" target="_blank">
-                        <img src={require("../assets/CaraCara-example-2.png")}></img>
-                        <img src={require("../assets/CaraCara-example-3.png")}></img>
-                    </a>
+                    <div className="img-background">
+                        <Link href="http://18.119.156.5/index.html#home_jump" target="_blank">
+                            <img src={require("../assets/CaraCara-example-2.png")}></img>
+                        </Link>
+                    </div>
+
+                    <div className="separator"></div>
+                    
+                    <div className="img-background">
+                        <Link>
+                            <img src={require("../assets/CaraCara-example-3.png")}></img>
+                        </Link>
+                    </div>
                 </div>
     
                 <div>
--- src/components/Display-uToob.jsx
+++ src/components/Display-uToob.jsx
@@ -1,13 +1,24 @@
+import { Link } from "react-router-dom";
+
 const DisplayUToob = () => {
     return ( 
         <div className="project">
         <h3>uToob - A YouTube Parallel</h3>
             <div className="prj-info">
                 <div>
-                    <a href="http://18.222.171.6/" target="_blank">
-                        <img src={require("../assets/uToob-example-1.png")}></img>
-                        <img src={require("../assets/uToob-example-2.png")}></img>
-                    </a>
+                    <div className="img-background">
+                        <Link href="http://18.222.171.6/" target="_blank">
+                            <img src={require("../assets/uToob-example-1.png")}></img>
+                        </Link>
+                    </div>
+
+                    <div className="separator"></div>
+
+                    <div className="img-background">
+                        <Link href="http://18.222.171.6/" target="_blank">
+                            <img src={require("../assets/uToob-example-2.png")}></img>
+                        </Link>
+                    </div>
                 </div>
                 <div>
                     <p>Example text of an explaination for the project, same infor as the lorem ispum above simulates</p>
